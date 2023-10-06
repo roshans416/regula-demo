80b285b44f3ead0e3222e094f0faa0f5388d6654
roshans416 <roshans416@gmail.com>
2023-10-06 13:19:29 +0000

    Modified terrform main.tf

--- infra_tf/variables.tf
+++ infra_tf/variables.tf
@@ -1,9 +1,11 @@
 variable "project" {
     type= string
     description = "ID Google project"
+    default = "global-pn-solution-development"
 }
 
 variable "region" {
     type= string
     description = "Region Google project"
+    default = "asia-southeast1"
 }
