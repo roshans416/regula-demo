395fdc9a2ee48d8e872d2440b93286f8917cd205
roshans416 <roshans416@gmail.com>
2023-10-06 14:04:09 +0000

    Modified tf files and rego rule

--- infra_tf/main.tf
+++ infra_tf/main.tf
@@ -1,144 +1,88 @@
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 provider "google" {
-  project = "global-pn-solution-development"
-  region = "asia-southeast1"
-  zone = "asia-southeast1-a"
-}
-# [START storage_flask_google_cloud_quickstart_parent_tag]
-# [START compute_flask_quickstart_vpc]
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-custom-mode-network"
-  auto_create_subnetworks = false
-  mtu                     = 1460
+  project = var.project
+  region  = var.region
 }
 
-resource "google_compute_subnetwork" "default" {
-  name          = "my-custom-subnet"
-  ip_cidr_range = "10.0.4.0/24"
-  #region        = "asia-southeast1"
-  network       = google_compute_network.vpc_network.id
-}
-# [END compute_flask_quickstart_vpc]
+resource "google_compute_firewall" "firewall" {
+  name    = "gritfy-firewall-externalssh"
+  network = "default"
 
-# [START compute_flask_quickstart_vm]
-# Create a single Compute Engine instance
-resource "google_compute_instance" "default" {
-  name         = "flask-vm"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Not So Secure. Limit the Source Range
+  target_tags   = ["externalssh"]
+}
+
+resource "google_compute_firewall" "webserverrule" {
+  name    = "gritfy-webserver"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80","443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Not So Secure. Limit the Source Range
+  target_tags   = ["webserver"]
+}
+
+# We create a public IP address for our google compute instance to utilize
+resource "google_compute_address" "static" {
+  name = "vm-public-address"
+  project = var.project
+  region = var.region
+  depends_on = [ google_compute_firewall.firewall ]
+}
+
+
+resource "google_compute_instance" "dev" {
+  name         = "devserver"
   machine_type = "f1-micro"
-  #zone         = "asia-southeast1-a"
-  tags         = ["ssh"]
+  zone         = "${var.region}-a"
+  tags         = ["externalssh","webserver"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "centos-cloud/centos-7"
     }
   }
-
-  # Install Flask
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
 
   network_interface {
-    subnetwork = google_compute_subnetwork.default.id
+    network = "default"
 
     access_config {
-      # Include this section to give the VM an external IP address
+      nat_ip = google_compute_address.static.address
     }
   }
-}
-# [END compute_flask_quickstart_vm]
 
-# [START vpc_flask_quickstart_ssh_fw]
-resource "google_compute_firewall" "ssh" {
-  name = "allow-ssh"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
+  provisioner "remote-exec" {
+    connection {
+      host        = google_compute_address.static.address
+      type        = "ssh"
+      user        = var.user
+      timeout     = "500s"
+      private_key = file(var.privatekeypath)
+    }
+
+    inline = [
+      "sudo yum -y install epel-release",
+      "sudo yum -y install nginx",
+      "sudo nginx -v",
+    ]
   }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc_network.id
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh"]
-}
-# [END vpc_flask_quickstart_ssh_fw]
 
+  # Ensure firewall rule is provisioned before server, so that SSH doesn't fail.
+  depends_on = [ google_compute_firewall.firewall, google_compute_firewall.webserverrule ]
 
-# [START vpc_flask_quickstart_5000_fw]
-resource "google_compute_firewall" "flask" {
-  name    = "flask-app-firewall"
-  network = google_compute_network.vpc_network.id
-
-  allow {
-    protocol = "tcp"
-    ports    = ["5000"]
+  service_account {
+    email  = var.email
+    scopes = ["compute-ro"]
   }
-  source_ranges = ["0.0.0.0/0"]
-}
-# [END vpc_flask_quickstart_5000_fw]
 
-# Create new multi-region storage bucket in the US
-# with versioning enabled
-
-# [START storage_kms_encryption_tfstate]
-resource "google_kms_key_ring" "terraform_state" {
-  name     = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  location = "us"
-}
-
-resource "google_kms_crypto_key" "terraform_state_bucket" {
-  name            = "test-terraform-state-bucket"
-  key_ring        = google_kms_key_ring.terraform_state.id
-  rotation_period = "86400s"
-
-  lifecycle {
-    prevent_destroy = false
+  metadata = {
+    ssh-keys = "${var.user}:${file(var.publickeypath)}"
   }
 }
-
-# Enable the Cloud Storage service account to encrypt/decrypt Cloud KMS keys
-data "google_project" "project" {
-}
-
-resource "google_project_iam_member" "default" {
-  project = data.google_project.project.project_id
-  role    = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member  = "serviceAccount:service-${data.google_project.project.number}@gs-project-accounts.iam.gserviceaccount.com"
-}
-# [END storage_kms_encryption_tfstate]
-
-# [START storage_bucket_tf_with_versioning]
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "default" {
-  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-  encryption {
-    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
-  }
-  depends_on = [
-    google_project_iam_member.default
-  ]
-}
-# [END storage_bucket_tf_with_versioning]
-# [END storage_flask_google_cloud_quickstart_parent_tag]
--- infra_tf/variables.tf
+++ infra_tf/variables.tf
@@ -9,3 +9,17 @@ variable "region" {
     description = "Region Google project"
     default = "asia-southeast1"
 }
+variable "user" {
+    type = string
+}
+variable "email" {
+    type = string
+}
+variable "privatekeypath" {
+    type = string
+    default = "~/.ssh/id_rsa"
+}
+variable "publickeypath" {
+    type = string
+    default = "~/.ssh/id_rsa.pub"
+}
